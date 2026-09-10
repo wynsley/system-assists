135b1306b381f2e74c7319e71d23829046a2dc93
wynsley <wynsleyjhacksonduransalazar@gmail.com>
2026-09-09 23:07:06 -0500

    feat: Agregar estilos secundarios para la escala de comportamiento

--- frontend/src/config/assistant/behavior.js
+++ frontend/src/config/assistant/behavior.js
@@ -1,7 +1,16 @@
 export const behavior_scale_style = {
+  primary : {
   AD: "bg-blue-100 text-blue-700",
   A: "bg-green-100 text-green-700",
   B: "bg-yellow-100 text-yellow-700",
   C: "bg-red-100 text-red-700",
+  },
+  secondary : {
+  AD: "text-blue-700 ",
+  A: "text-green-700 ",
+  B: "text-yellow-700 ",
+  C: "text-red-700 ",
+  },
+
 }
 
